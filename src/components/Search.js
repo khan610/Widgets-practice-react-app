9e0f0fd3104b4274e320f07fabd9cac05b628c08
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-06-01 23:16:38 +0200

    Update

--- src/components/Search.js
+++ src/components/Search.js
@@ -19,7 +19,19 @@ const Search = () => {
       setResults(data.query.search);
     };
 
-    search();
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
