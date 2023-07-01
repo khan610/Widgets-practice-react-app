c2cb0bd07c44e964f4b8b969ff968fec293c526d
Andrea Trajkovic <trajkovicandrea@gmail.com>
2023-07-01 17:32:19 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -35,5 +37,27 @@ const options = [
 ];
 
 export default () => {
-  return <Translate />;
+  const [selected, setSelected] = useState(options[0]);
+  return (
+    <div>
+      <Header />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          option={options}
+          selected={selected}
+          onSelectedCahnge={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  );
 };
